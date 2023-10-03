b4c1933a1d35d767fb43754da12e6fa049ef3dfc
Marcus Edlund <marcus.edlund@yh.nackademin.se>
2023-10-03 17:16:53 +0200

    Initial commit

---
+++ src/main/java/com/example/carpooltaxi/CarpooltaxiApplication.java
@@ -0,0 +1,13 @@
+package com.example.carpooltaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpooltaxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarpooltaxiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carpooltaxi/CarpooltaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carpooltaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpooltaxiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
